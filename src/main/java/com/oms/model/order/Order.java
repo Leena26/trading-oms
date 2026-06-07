8fae6517b869df31c3f2bc3b2b746b026a49c076
Leena Dany <67622202+Leena26@users.noreply.github.com>
2026-06-07 15:14:06 +0100

    Add all getters and setters to Order Class

---
+++ src/main/java/com/oms/model/order/Order.java
@@ -0,0 +1,47 @@
+package order;
+import order.OrderStatus;
+
+public abstract class Order{
+    private String orderId;
+    private String traderId;
+    private String symbol;
+    private int quantity;
+    private OrderStatus status;
+
+    public Order(String orderId, String traderId, String symbol, int quantity){
+        this.orderId = orderId;
+        this.traderId = traderId;
+        this.symbol = symbol;
+        this.quantity = quantity;
+        this.status= OrderStatus.PENDING;
+    }
+
+    public String getOrderId(){
+        return this.orderId;
+    }
+
+    public String getTraderId(){
+        return this.traderId;
+    }
+
+    public String getSymbol(){
+        return this.symbol;
+    }
+
+    public int getQuantity(){
+        return this.quantity;
+    }
+
+    public OrderStatus getStatus(){
+        return this.status;
+    }
+
+    public void setStatus(OrderStatus s){
+        this.status = s;
+    }
+
+    public void setQuantity(int q){
+        this.quantity = q;
+    }
+    public abstract boolean validate();
+}
