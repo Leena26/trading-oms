d6078212d6c999ae3952a688dc3092100504df4b
Leena Dany <67622202+Leena26@users.noreply.github.com>
2026-06-07 15:27:17 +0100

    StopOrder Class

---
+++ src/main/java/com/oms/model/order/StopOrder.java
@@ -0,0 +1,20 @@
+package order;
+import order.Order;
+
+public class StopOrder extends Order{
+    private double stopPrice;
+
+    public StopOrder(String orderId, String traderId, String symbol, int quantity, double stopPrice){
+        super(orderId, traderId, symbol, quantity);
+        this.stopPrice = stopPrice;
+    }
+
+    public double getStopPrice(){
+        return this.stopPrice;
+    }
+
+    @Override
+    public boolean validate(){
+        return this.getQuantity() > 0 && this.stopPrice > 0;
+    }
+}
