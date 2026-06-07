f3e2f367b40245db1eb2ca1dfb141e064533060e
Leena Dany <67622202+Leena26@users.noreply.github.com>
2026-06-07 15:27:49 +0100

    Limit Order Class

---
+++ src/main/java/com/oms/model/order/LimitOrder.java
@@ -0,0 +1,20 @@
+package order;
+import order.Order;
+
+public class LimitOrder extends Order{
+    private double limitPrice;
+
+    public LimitOrder(String orderId, String traderId, String symbol, int quantity, double limitPrice){
+        super(orderId, traderId, symbol, quantity);
+        this.limitPrice = limitPrice;
+    }
+
+    public double getLimitPrice(){
+        return this.limitPrice;
+    }
+
+    @Override
+    public boolean validate(){
+        return this.getQuantity() > 0 && this.limitPrice > 0;
+    }
+}
